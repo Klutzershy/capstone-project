1466cb69fac3feba0dd20d8a76dc404b8f6f481f
Patrick Curiale <patrickcuriale@gmail.com>
2018-02-23 02:37:48 -0500

    Raw Data
    I got the text results working so now I have the raw xml data in the places.php file

--- googleMaps/places.php
+++ googleMaps/places.php
@@ -1,1 +1,35 @@
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCJcbdw_nWmu-ZogZc0TbOPDWDKVDCd3MQ&libraries=places"></script>
+<?php
+$url = "https://maps.googleapis.com/maps/api/place/textsearch/xml?query=theaters+in+Philadelphia&key=AIzaSyCJcbdw_nWmu-ZogZc0TbOPDWDKVDCd3MQ";
+
+function get_xml( $url )
+{
+    //found some options on stack overflow
+    $options = array(
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HEADER         => false,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_ENCODING       => "",
+        CURLOPT_AUTOREFERER    => true,
+        CURLOPT_CONNECTTIMEOUT => 120,
+        CURLOPT_TIMEOUT        => 120,
+        CURLOPT_MAXREDIRS      => 10,
+        CURLOPT_SSL_VERIFYPEER => false
+    );
+
+    $ch      = curl_init( $url );
+    curl_setopt_array( $ch, $options );
+    $content = curl_exec( $ch );
+    $err     = curl_errno( $ch );
+    $errmsg  = curl_error( $ch );
+    $header  = curl_getinfo( $ch );
+    curl_close( $ch );
+
+    $header['errno']   = $err;
+    $header['errmsg']  = $errmsg;
+    $header['content'] = $content;
+    header('Content-type: application/xml');
+    return $header['content'];
+}
+
+echo get_xml($url);
+?>
