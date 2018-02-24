4da035aebd21c6cc2000429ff62f7548e5dfdeaa
PatFoltyn <patfoltyn@yahoo.com>
2018-02-24 00:29:22 -0500

    Fix donate.php to show raw data

--- donate.php
+++ donate.php
@@ -1,56 +1,33 @@
-<link rel="stylesheet" type="text/css" href="projectCss.css">
 
 
-<div class = "donateWrapper">
-	<div class = "donateForm">
-
-
-
-
-		<div class = "donateInner">
 
 <?php
 
+//Patrick Foltyn 		2/23/2018
+//Capstone Project
+
 if(!empty($_GET['search'])){
-	$search_url = 'https://api.themoviedb.org/3/search/movie?api_key=2b026bda7ea9b58930161475b7d89a62&language=en-US&query=' .  urlencode($_GET['search']) . '&page=1&include_adult=false';
+	$search_url = 'https://api.themoviedb.org/3/search/movie?api_key=2b026bda7ea9b58930161475b7d89a62&language=en-US&query=' . urlencode($_GET['search']) . '&page=1&include_adult=false';
 
 	$search_json = file_get_contents($search_url);
-	$search_array = json_decode($search_json, true);
+	$search_array = json_decode($search_json);
 }
 
 ?>
 
-<h1> The Movie Database API Search </h1>
 
-<p> Search for movie by title </p>
 
 <form action = "./donate.php">
 	<input type = "text" name = "search"/>
 	<button type = "submit">search</button>
 	<br>
-	<?php
-	$prefix = "https://cf2.imgobject.com/t/p/w500";
-
-		foreach($search_array[results] as $movie){
-				$s.="<p><img src= $prefix.$movie[poster_path] height=80 width=60/></p>";
-				$s.="<p>Title: $movie[original_title]<br>Release Date: $movie[release_date]</p>";
-
-
-			}
-
-		echo $s;
-
-
-	?>
 </form>
 
-</div>
 
+<?php
 
-
-
-
-
-
-	</div>
-</div>
+	echo "You are searching for movies with the name: " . $_GET['search'];
+	echo '<pre>';
+	print_r($search_array);
+	echo '</pre>';
+?>
