62855934f7995f3dfd9e2d36331d0b84c61b0bfc
Patrick Curiale <patrickcuriale@gmail.com>
2018-04-13 17:05:20 -0400

    map
    put in my map into profile.php

--- profile.php
+++ profile.php
@@ -1,19 +1,23 @@
-<?php
-  session_start();
- ?>
 <div class="container">
   <div class="row">
         <div class="col-md-6">
-          <h4><?php echo $_SESSION['sess_username'];?></h4>
-          <small><cite><?php echo $_SESSION['sess_location']; ?> </cite></small>
+          <img src="<?php echo $_SESSION['sess_profImg']; ?>" alt="Profile Pic">
+          <?php
+            echo $_SESSION['sess_username'];
+          ?>
         </div>
         <div class="col-md-6">
 
-        </div>
     </div>
     <hr>
-    <div class="row">
-        <div class="col-md-6"></div>
+    <div class="row" style="height:350px;width:1200px;">
+        <div class="col-md-12" style="height:100%;width:100%;">
+            <?php
+               include 'googleMaps/map.php';
+             ?>
+
+        </div>
+
         <div class="col-md-6"></div>
     </div>
 </div>
